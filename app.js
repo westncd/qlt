e2bbe7bb3f02460b9b7318215bb10443267fb60d
Ân <69793665+jian131@users.noreply.github.com>
2023-12-15 23:49:00 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-  $('#login').submit((e) {
+// Hàm xử lý submit form login
+$("#loginForm").submit(function(event) {
 
-    e.preventDefault();
+  // Ngăn chặn submit mặc định
+  event.preventDefault();
 
-    let username = $('#username').val(); 
-    let password = $('#password').val();
+  // Lấy dữ liệu người dùng nhập
+  var username = $("#username").val(); 
+  var password = $("#password").val();
 
-    if(username == 'admin' && password == '1234') {
-      window.location.href = 'home.html';
+  // Kiểm tra dữ liệu hợp lệ
+  if(username && password) {
+
+    // Kiểm tra quyền người dùng
+    if(username == "host" && password == "1234") {
+    
+      // Chuyển trang nếu là chủ trọ  
+      window.location.href = "/host.html";
+
+    } else if(username == "tenant" && password == "1234") {
+
+      // Chuyển trang nếu là người thuê
+      window.location.href = "/home.html";
+
     } else {
-      alert('Sai tài khoản hoặc mật khẩu');
+      // Hiển thị thông báo lỗi nếu tên đăng nhập không hợp lệ
+      alert("Invalid username"); 
     }
 
-  });
-
-  function showHostLogin() {
-    document.getElementById("host-login").style.display = "block";  
-    document.getElementById("tenant-login").style.display = "none";
+  } else {
+    alert("Vui lòng nhập đầy đủ thông tin");
   }
-
-  function showTenantLogin() {
-    document.getElementById("tenant-login").style.display = "block";
-    document.getElementById("host-login").style.display = "none"; 
-  }
-
-  function loginHost() {
-    let id = document.getElementById("host-id").value;
-    window.location.href = "host.html?id=" + id; 
-  }
-
-  function loginTenant() {
-    let motelId = document.getElementById("motel-id").value; 
-    let roomNo = document.getElementById("room-no").value;
-    window.location.href = "tenant.html?motelid=" + motelId + "&roomno=" + roomNo;
-  }
-
+  
+});
